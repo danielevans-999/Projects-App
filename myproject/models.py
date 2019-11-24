b3cc111b4b15a90091caf1dfd880050dc87b4fd1
danielevans-999 <danielevans.karani@gmail.comgit>
2019-11-25 00:33:35 +0300

    creates two models the post models and the reviews models

--- myproject/models.py
+++ myproject/models.py
@@ -13,3 +13,19 @@ class Profile(models.Model):
     profile_pic = models.ImageField(upload_to='images/' , default='images/smoke.jpeg')
     user = models.OneToOneField(User,on_delete=models.CASCADE,primary_key=True)
 
+class Post(models.Model):
+    title =  models.CharField(max_length=30)
+    image = models.ImageField(upload_to='images/')
+    description = HTMLField()
+    link = models.CharField(max_length=500)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    
+class Reviews(models.Model):
+    title = models.CharField(max_length=50)
+    review = models.TextField()
+    design = models.PositiveIntegerField(default=0)
+    usability = models.PositiveIntegerField(default=0)
+    content = models.PositiveIntegerField(default=0)
+    post = models.ForeignKey(Post,on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    
