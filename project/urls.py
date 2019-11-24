3f206eb7b4da9e6cfc9a5d239389f9f13add85e8
danielevans-999 <danielevans.karani@gmail.comgit>
2019-11-25 00:57:58 +0300

    fixes indentation

--- project/urls.py
+++ project/urls.py
@@ -18,7 +18,8 @@ from django.contrib import admin
 from django.contrib.auth import views
 
 urlpatterns = [
+    
     url(r'^admin/', admin.site.urls),
     url(r'',include('myproject.urls')),
-     url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
 ]
