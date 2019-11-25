283d9d01c9a16c2192cee5bb7ae255eae7aa54ea
danielevans-999 <danielevans.karani@gmail.comgit>
2019-11-25 08:23:08 +0300

    change the url path mapped to the home page

--- myproject/views.py
+++ myproject/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render,redirect
 
 def home(request):
-    return redirect('accounts/register')
-
-def projects(request):
-    
     return render(request,'myprojects/index.html')
+
+    
+    
