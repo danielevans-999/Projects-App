a9b6768424859b13906e5cffa13a6667ba496f63
danielevans-999 <danielevans.karani@gmail.comgit>
2019-11-25 08:15:08 +0300

    Adds a navbar

--- myproject/urls.py
+++ myproject/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns =  [
     
     url(r'^$',views.home,name='home'),
+    url(r'^projects/home/$', views.projects,name='projects')
 ]
--- myproject/views.py
+++ myproject/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render,redirect
 
 def home(request):
     return redirect('accounts/register')
+
+def projects(request):
+    
+    return render(request,'myprojects/index.html')
